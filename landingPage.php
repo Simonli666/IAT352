426a7951f7217a005366b1f990f3b9f869431824
umbrella-afk <wtingw@qq.com>
2019-10-02 18:14:39 -0700

    3

--- landingPage.php
+++ landingPage.php
@@ -1,108 +1,90 @@
 <!DOCTYPE html>
 <html>
-
-<!--  import bootstrap  -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-
   <head>
+    <meta charset="utf-8">
+    <title> Movie Web</title>
+    <script src="https://code.jquery.com/jquery-3.4.1.js">
+  </script>
+    <script type= "text/javascript">
+      $(document).ready(function(){
+        $('.sidebarBtn').click(function(){
+          $('.sidebar').toggleClass('active');
+          $('.sidebarBtn').toggleClass('toggle');
 
-    <title>Movie Web</title>
-    <!-- <link rel="stylesheet" href="./css/style.css"> -->
+
+        })
+
+      })
+    </script>
+
+    <link rel="stylesheet" href="style.css">
   </head>
 
+<body>
 
-  <body>
-    <header>
-      <div class="container">
-      <div class="page-header">
-        <div>
-          <img src="./img/logo.jpg">
-          <!-- title and logo -->
-          <h1><span class="highlight">Movie</span> Web</h1>
-        </div>
-        <nav>
-          <ul>
-            <li class="current"><a href="home_page.php">Home</a></li>
-            <li><a href="">Genre</a></li>
-            <li><a href="signUp.php">Sign Up</a></li>
-          </ul>
-
-          <form action="/action_page.php">
+  <!-- hearder -->
+  <header>
+    <div class="container">
+    <div id="branding">
+      <div>
+        <img src="./img/logo.jpg">
+        <!-- title and logo -->
+        <h1><span class="highlight">Movie</span> Web</h1>
+      </div>
+      <nav>
+        <ul>
+          <li class="current"><a href="home_page.php">Home</a></li>
+          <li><a href="">Genre</a></li>
+          <li><a href="signUp.php">Sign Up</a></li>
+          <li><form action="/action_page.php">
             <input type="text" placeholder="Search.." name="search">
             <button type="submit"><i class="fa fa-search"></i></button>
           </form>
-
-        </nav>
-      </div>
-      </div> -->
-    </header>
+        </li>
+        </ul>
 
 
 
-    <section>
-      <div class="container">
-        <a href="">  <h1>Top Rate Collection</h1><img src="./img/ph.jpg"></a>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu luctus ipsum, rhoncus semper magna. Nulla nec magna sit amet sem interdum condimentum.</p>
-      </div>
-    </section>
+      </nav>
+    </div>
+    </div>
+  </header>
 
 
 
-    <section>
-      <div class="container">
-        <div>
-          <a href="">  <h3>Comedy</h3><img src="./img/ph.jpg"></a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
-        </div>
-
-        <div>
-          <a href="">  <h3>Horror</h3><img src="./img/ph.jpg"></a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
-        </div>
-
-        <div>
-          <a href="">  <h3>Romance</h3>  <img src="./img/ph.jpg"></a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
-        </div>
-
-        <div >
-            <a href="">  <h3>Drama</h3>  <img src="./img/ph.jpg"></a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
-        </div>
-
-        <div>
-          <a href="">  <h3>Fiction</h3>  <img src="./img/ph.jpg"></a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
-        </div>
-
-        <div>
-          <a href="">  <h3>Action</h3>  <img src="./img/ph.jpg"></a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
-        </div>
-
-        <div>
-          <a href="">  <h3>Documentary</h3>  <img src="./img/ph.jpg"></a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
-        </div>
-
-        <div>
-            <a href="">  <h3>Advanture</h3>  <img src="./img/ph.jpg"></a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p></br>
-        </div>
-
-
-    </section>
-
-    <footer>
-      <p>movie web, Copyright &copy; 2019</p>
-    </footer>
 
 
 
-    <!-- bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  </body>
+
+
+
+
+
+
+  <div class="sidebar">
+    <ul>
+      <li><a href="#">Comedy</a></li>
+      <li><a href="#">Horror</a></li>
+      <li><a href="#">Romance</a></li>
+      <li><a href="#">Drama</a></li>
+      <li><a href="#">Fiction</a></li>
+      <li><a href="#">Action</a></li>
+      <li><a href="#">Documentary</a></li>
+      <li><a href="#">Advanture</a></li>
+    </ul>
+
+    <button class="sidebarBtn">
+      <span></span>
+    </button>
+
+
+  </div>
+
+</body>
+
+
+
+
+
+
 </html>
