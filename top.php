9586fafe0fae2073d2d6d0f99aaa600244b0e719
umbrella-afk <wtingw@qq.com>
2019-10-03 13:17:32 -0700

    1

---
+++ top.php
@@ -0,0 +1,6 @@
+<section>
+  <div class="topCol">
+    <a href="top.php?page = drama">  <h2>Top Rate Collection</h2><img src="./img/ph.jpg" ></a>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu luctus ipsum, rhoncus semper magna. Nulla nec magna sit amet sem interdum condimentum.</p>
+  </div>
+</section>
