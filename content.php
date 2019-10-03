10ecb3a60859b2952933103d6f3430bfea9fd73a
bobo <liyunbo11123@gmail.com>
2019-10-03 13:05:46 -0700

    content page

--- content.php
+++ content.php
@@ -5,6 +5,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Content Page</title>
 
+ <link rel="stylesheet" href="css/content.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
@@ -13,91 +14,154 @@
 
   <body> 
 
-    <div class="container">
-<!--    <div class =" col-md-8" align="center">  -->
+    <div class="container-fluid ">
 
-      <div class= "row title">  
+      <div class = "col-md-3"> 
 
-        <div class = "col-md-8">   
-          <h1> TITLE: Movie Name 
+      </div>
+
+   <div class =" col-md-6" > 
+
+              <div class= "row title">  
+
+                <div class = "col-md-12">   
+                  <h1> TITLE: Movie Name 
+                    
+                  </h1> <br>
+          
+              </div>
+
+                  <!-- <div class = "col-md-4" style="  position:relative; bottom:0; right:0;">   
+                  <p> rating: 9/10 </p>
+
+                  </div> -->
+              </div>
+
+              <div class= "row dis">  
+
+                 <div class = "col-md-8">    <p> <a href=""> Genre </a>  | Duration:3h   | Year:  1999  </p> </div>
+
+                  <div class = "col-md-4" align="right">    <p> rating: 9/10 </p> </div>
+
+              </div>
+
+
+
+                <div class = "row images"> 
+                      <div class="col-md-12">  
+                       <img class ="img-responsive" style="width:100%; height:auto;" src="img/ph.jpg" >
+
+                      </div>
+
+                    <!--   <div class="col-md-4" >  
+                       <img class ="img-responsive" style="width:100%; height:auto;"src="./img/ph.jpg" >  
+                        <img class ="img-responsive" style="width:100%; height:auto;" src="./img/ph.jpg" >
+
+                      </div> -->
+
+                </div>
+
+
+               <div class = "row discription"> 
+
+                 <div class="col-md-9">  
+
             
-          </h1> <br>
-          <p> <a href=""> Genre </a>  | Duration:3h  | Year:  1999  </p>
-      </div>
+                   <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc </p>
 
-          <div class = "col-md-4">   
-          <p> rating: 9/10 </p>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc </p>
 
-          </div>
-      </div>
+                </div>
+
+                <div class="col-md-3">
 
 
+                </div>
 
-      <div class = "row image"> 
-        <div class="col-md-12">  
-        <a href="">  <img src="./img/ph.jpg" ></a>
+              </div>
 
-        </div>
+               <div class = "row buttons"> 
 
-      </div>
+          
 
+                    <div class= "col-md-6">  
+                      <button type="button" class="btn btn-primary">Add to WishList</button>
 
-       <div class = "row discription"> 
-
-         <div class="col-md-6">  
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc </p>
-
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc </p>
-
-           <button type="button" class="btn btn-primary">Add to WishList</button>
-
-        </div>
-
-      </div>
+                    </div>
+               </div>
 
 
-      <div class = "row storyline"> 
+                <div class = "row storyline"> 
 
-          <div class="col-md-12"> 
-            <h2> Storyline: </h2>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc omnibus fidibus, ut incontentae sint. Tamen aberramus a proposito, et, ne longius, prorsus, inquam, Piso, si ista mala sunt, placet. Apud imperitos tum illa dicta sunt, aliquid etiam coronae datum; </p>
+                    <div class="col-md-12"> 
+                      <h2> Storyline: </h2>
+                      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc omnibus fidibus, ut incontentae sint. Tamen aberramus a proposito, et, ne longius, prorsus, inquam, Piso, si ista mala sunt, placet. Apud imperitos tum illa dicta sunt, aliquid etiam coronae datum; </p>
 
-          </div>
+                    </div>
 
 
 
-      </div>
+                </div>
 
 
-      <div class = "row comment"> 
+                  <div class="row rating"> 
 
-          <div class ="col-md-12"> 
-            <h4> 12 Comments </h4>
-             
+                    <div class="col-md-12">  
+                    <h2>  Rating:</h2>
+                    <div class="rate">
+                      <input type="radio" id="star5" name="rate" value="5" />
+                      <label for="star5" title="text">5 stars</label>
+                      <input type="radio" id="star4" name="rate" value="4" />
+                      <label for="star4" title="text">4 stars</label>
+                      <input type="radio" id="star3" name="rate" value="3" />
+                      <label for="star3" title="text">3 stars</label>
+                      <input type="radio" id="star2" name="rate" value="2" />
+                      <label for="star2" title="text">2 stars</label>
+                      <input type="radio" id="star1" name="rate" value="1" />
+                      <label for="star1" title="text">1 star</label>
+                    </div>
 
-
-          </div>
-
-
-          <div class ="col-md-10"> 
-            <textarea rows="4" cols="100">
-
-
-            </textarea>
-            
-
-          </div>
-
-
-          <div class ="col-md-2"> 
-            <button type="button" class="btn btn-default">Post a Comment</button> </h4>
+                  </div>
 
 
-          </div>
-           
+                </div>
 
 
-      </div>
+                <div class = "row comment"> 
+
+                    <div class ="col-md-12"> 
+                      <h4> 12 Comments </h4>
+                       
+
+
+                    </div>
+
+                  
+
+
+                    <div class ="col-md-12"> 
+                      <textarea rows="4" cols="93">
+
+
+                      </textarea>
+                      
+
+                    </div> 
+                      <div class ="col-md-12" align ="right"> 
+                      <button type="button" class="btn btn-default">Post a Comment</button> 
+
+
+                    </div>
+
+
+                    
+                     
+
+
+                </div>
+
+
+
 
 
 
@@ -106,7 +170,11 @@
 
    </div>
 
-<!--  </div> -->
+       <div class = "col-md-3"> 
+
+      </div>
+
+ </div>
 
 
 
@@ -116,3 +184,6 @@
 
 
   </html>
+
+
+
